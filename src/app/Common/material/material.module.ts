ed15375b637a0bfa44bddb834995b660e0e04c3c
robertoleiton <rleiton@clavisco.com>
2023-02-03 15:47:18 -0600

    Estilos css

--- src/app/Common/material/material.module.ts
+++ src/app/Common/material/material.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 
-
+const modulos = [
+  MatToolbarModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatSidenavModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MatCheckboxModule
-  ],
-  exports: [
-    MatCheckboxModule
-  ]
+  imports: modulos,
+  exports: modulos
 })
 export class MaterialModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,19 +8,19 @@ import { ProtectionRouteGuard } from '@Guards/protection-route.guard';
 const routes: Routes = [
   // { path: '', redirectTo: 'Home', component: AppComponent, pathMatch: 'full' },
   {
-    canActivate: [ProtectionRouteGuard],
+
     path: 'Checkbox',
     component: CheckboxComponent,
     title: 'Checkbox'
   },
   {
-    canActivate: [ProtectionRouteGuard],
+
     path: 'Formularios',
     component: FormReactivesComponent,
     title: 'Formularios'
   },
   {
-    canActivate: [ProtectionRouteGuard],
+
     path: 'Standalone',
     loadComponent: () => import('@Component/standalone/standalone.component')
       .then(c => c.StandaloneComponent),
